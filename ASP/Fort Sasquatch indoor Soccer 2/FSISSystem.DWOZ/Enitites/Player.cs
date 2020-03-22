c464aa6034fc251ee68971a7969bce5df9f0451c
dylan <dylanwozny14@gmail.com>
2020-03-21 19:46:24 -0600

    done entities

--- ASP/Fort Sasquatch indoor Soccer 2/FSISSystem.DWOZ/Enitites/Player.cs
+++ ASP/Fort Sasquatch indoor Soccer 2/FSISSystem.DWOZ/Enitites/Player.cs
@@ -12,7 +12,72 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FSISSystem.DWOZ.Enitites
 {
-    class Player
+    [Table("Player")]
+    public class Player
     {
+        //---------------------fully implemented-----------------------------------------
+
+        private string _gender;
+        private string _medicalalertdetails;
+
+
+        //--------------------------------------------------------------
+        [Key]
+        public int PlayerID { get; set; }
+
+        //--------------------------------------------------------------
+        [Required(ErrorMessage = "GuardianID is required.")]
+        public int GuardianID { get; set; }
+
+
+        //--------------------------------------------------------------
+        [Required(ErrorMessage = "TeamID is required.")]
+        public int TeamID{ get; set; }
+        
+        //--------------------------------------------------------------
+
+        [Required(ErrorMessage = "Player name is required")]
+        [StringLength(50, ErrorMessage = "string must be under 50 characters")]
+        public string FirstName { get; set; }
+
+        //--------------------------------------------------------------------
+
+        [Required(ErrorMessage = "last name of player is required")]
+        [StringLength(50, ErrorMessage = "string must be under 50 characters")]
+
+        public string LastName { get; set; }
+
+        //--------------------------------------------------------------
+        [Required(ErrorMessage = "Age is required.")]
+        public int Age{ get; set; }
+
+        //--------------------------------------------------------------
+        [Required(ErrorMessage = "Gender is required.")]
+        public string Gender {
+            get { return _gender; }
+            set { _gender = _gender.ToUpper(); }          
+        }
+
+        //--------------------------------------------------------------
+        [Required(ErrorMessage = "Alberts health care number is required.")]
+        public string AlbertaHealthCareNumber { get; set; }
+
+        //--------------------------------------------------------------
+        [Required(ErrorMessage = "medical alert details are required.")]
+        [StringLength(250, ErrorMessage = "mdeical alert description must be under 250 characters")]
+        public string MedicalAlertDetails
+        {
+            get { return _medicalalertdetails; }
+            set { _medicalalertdetails = string.IsNullOrEmpty(value) ? null : value; }
+        }
+
+        //--------------------------------------------------------------------
+        [NotMapped]
+
+        public string FullName
+        {
+            get { return FirstName + "," + "" + LastName; }
+        } 
+
     }
 }
---
+++ ASP/Fort Sasquatch indoor Soccer 2/FSISSystem.DWOZ/Enitites/Team.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// additional namespaces......
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace FSISSystem.DWOZ.Enitites
+{
+    [Table("Team")]
+    public class Team
+    {
+        private string _wins;
+        private string _losses; 
+
+
+        //-----------------------------------------------------------------------
+        [Key]
+        public int TeamID { get; set; }
+
+        //--------------------------------------------------------------------
+
+        [Required(ErrorMessage = "Team name is required")]
+        [StringLength(50, ErrorMessage = "string must be under 50 characters")]
+        public string TeamName { get; set; }
+
+        //--------------------------------------------------------------------
+
+        [Required(ErrorMessage = "Coach is required")]
+        [StringLength(75, ErrorMessage = "string must be under 75 characters")]
+
+        public string Coach { get; set; }
+
+        //--------------------------------------------------------------------
+
+        [Required(ErrorMessage = "Assistant Coach is required")]
+        [StringLength(75, ErrorMessage = "string must be under 75 characters")]
+
+        public string AssistantCoach { get; set; }
+
+        //--------------------------------------------------------------------
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(10, ErrorMessage = "string must be under 10 characters")]
+
+        public string EmailAddress { get; set; }
+
+        //--------------------------------------------------------------
+        [Required(ErrorMessage = "wins are required.")]
+        public string Wins
+        {
+            get { return _wins; }
+            set { _wins= string.IsNullOrEmpty(value) ? null : value; }
+        }
+
+        //--------------------------------------------------------------
+        [Required(ErrorMessage = "Losses are required.")]
+        public string Losses
+        {
+            get { return _losses; }
+            set { _losses = string.IsNullOrEmpty(value) ? null : value; }
+        }
+
+
+    }
+}
+
+
+
+//   [Wins] [int] NULL,
+//	[Losses] [int] NULL,
