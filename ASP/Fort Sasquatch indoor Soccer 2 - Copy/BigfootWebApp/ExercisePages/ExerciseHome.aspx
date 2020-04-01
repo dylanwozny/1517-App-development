19ed80cf1590aacacfdca4e902023d43c513e722
dylan <dylanwozny14@gmail.com>
2020-03-31 20:38:59 -0600

    dd

---
+++ ASP/Fort Sasquatch indoor Soccer 2 - Copy/BigfootWebApp/ExercisePages/ExerciseHome.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ExerciseHome.aspx.cs" Inherits="BigfootWebApp.ExercisePages.ExerciseHome" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>
+        Fort Sasquatch Indoor Soccer
+    </h1>
+    <h2>Known Problems</h2>
+    <ul>
+        <li>No Problems</li>
+    </ul>
+    <asp:Image  src="../img/ERD.jpg" ID="Image1" runat="server" />
+</asp:Content>
---
+++ ASP/Fort Sasquatch indoor Soccer 2/FSISSystem.DWOZ/DAL/FSISSystemContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using FSISSystem.DWOZ.Enitites;
+using System.Data.Entity;
+#endregion
+
+namespace FSISSystem.DWOZ.DAL
+{
+    internal class FSISSystemContext:DbContext
+    {
+
+        public FSISSystemContext():base("FSIS_db")
+        {
+
+        }
+
+        public DbSet<Guardian> Guardians { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+
+
+    }
+}
